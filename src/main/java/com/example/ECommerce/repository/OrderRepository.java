67af412b26fef9db0677349a3a693d445ad28e32
cksdn1223 <rlacksdn1223@naver.com>
2025-10-08 18:55:45 +0900

    모든 주문을 조회하는 메서드 추가

--- src/main/java/com/example/ECommerce/repository/OrderRepository.java
+++ src/main/java/com/example/ECommerce/repository/OrderRepository.java
@@ -3,5 +3,8 @@ package com.example.ECommerce.repository;
 import com.example.ECommerce.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findAllByAppUserId(Long id);
 }
